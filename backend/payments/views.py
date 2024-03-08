7c52c7349b6daddff51df16e7907eef79776659f
Lucas <lucassavioli@gmail.com>
2024-03-08 11:40:48 -0300

    Inital commit

---
+++ backend/payments/views.py
@@ -0,0 +1,24 @@
+import stripe
+stripe.api_key = 'sk_test_Ho24N7La5CVDtbmpjc377lJI'
+
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': '{{PRICE_ID}}',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=YOUR_DOMAIN + '?success=true',
+            cancel_url=YOUR_DOMAIN + '?canceled=true',
+        )
+    except Exception as e:
+        return str(e)
+
+    return redirect(checkout_session.url, code=303)
+
+if __name__ == '__main__':
